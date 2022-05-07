51775ba1ea44c20c0d05ce7526c982a752672b7b
kemd <kemd91@gmail.com>
2022-05-07 11:23:06 -0400

    added a randomize button and created jQuery onclick function

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 function handleGetData() {
 
     $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
+        url: 'https://randomuser.me/api/', // API
+        dataType: 'json', // returned data
         success: function(data) {
             // console.log(data)
             $('div').append(`<h1> Gender: ${data["results"][0]["gender"]}</h1>`)
+        },
+        error: function(error) {
+            console.log('something went wrong', error)
         }
     })
 }
+
+//button
+$('#random').on('click', handleGetData)
