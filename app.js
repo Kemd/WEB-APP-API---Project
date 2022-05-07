9aa3f44575cf4ad7305b2127b3351d66c00db963
kemd <kemd91@gmail.com>
2022-05-07 13:17:57 -0400

    added name, gender, location, img & age data under apiData function

--- app.js
+++ app.js
@@ -23,5 +23,26 @@ $('#random').on('click', handleGetData)
 
 //function that handle all the incoming data from the API
 const apiData = () => {
-    $('div').append(`<h1> Gender: ${incData['results'][0]['gender']}</h1>`)
+    //name data
+    $('#name').text(`Name: ${incData['results'][0]['name']['title']} ${incData['results'][0]['name']['first']} ${incData['results'][0]['name']['last']}`)
+
+    // gender data
+    $('#gender').text('Gender: ' + incData['results'][0]['gender'])
+
+    // location data
+    $('#location').text(`Location: ${incData['results'][0]['location']['state']}, ${incData['results'][0]['location']['country']}`)
+
+    // image data
+    $('.flex-2').append(`<img src='${incData['results'][0]['picture']['large']}' />`)
+
+    // age data
+    $('#age').text(`Age: ${incData['results'][0]['dob']['age']} years old!`)
 }
+
+// function enterVoid () {
+//     const body = $('body')
+//     body.css({
+//         'background': "url(https://mymodernmet.com/wp/wp-content/uploads/2014/02/3d-gifs-3.gif)",
+//         'background-repeat': 'no-repeat'
+//     })
+// }
